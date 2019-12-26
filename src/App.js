4c8a33e5f29f9922fbf0d980d98fdedcaf404d7e
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2019-12-26 16:26:04 +0300

    feat(init): inital commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import connect from '@vkontakte/vk-connect';
+import View from '@vkontakte/vkui/dist/components/View/View';
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Home from './panels/Home';
+import Sokol from './panels/Sokol';
+
+const App = () => {
+	const [activePanel, setActivePanel] = useState('home');
+	const [fetchedUser, setUser] = useState(null);
+	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+
+	useEffect(() => {
+		connect.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				const schemeAttribute = document.createAttribute('scheme');
+				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+				document.body.attributes.setNamedItem(schemeAttribute);
+			}
+		});
+		async function fetchData() {
+			const user = await connect.sendPromise('VKWebAppGetUserInfo');
+			setUser(user);
+			setPopout(null);
+		}
+		fetchData();
+	}, []);
+
+	const go = e => {
+		setActivePanel(e.currentTarget.dataset.to);
+	};
+
+			// Galaxy Dragon Code
+
+			// End Galaxy Dragon Code
+
+	return (
+		<View activePanel={activePanel} popout={popout}>
+			<Home id='home' fetchedUser={fetchedUser} go={go} />
+			<Sokol id='sokol' go={go} />
+		</View>
+	);
+}
+
+export default App;
+
