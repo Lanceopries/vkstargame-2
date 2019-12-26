8fd658a9a0fe7109255dd2ce347d149a93f6a83b
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2019-12-26 16:23:52 +0300

    Updates

---
+++ precache-manifest.0ce3d1133dcfcac5e12cd8beb165a8ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e95d37f70bf33855701ee9465ec70e",
+    "url": "/vkstargame-2/index.html"
+  },
+  {
+    "revision": "7aa8744261199131a053",
+    "url": "/vkstargame-2/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "2b27df99d06141966c03",
+    "url": "/vkstargame-2/static/css/main.55b86c5f.chunk.css"
+  },
+  {
+    "revision": "7aa8744261199131a053",
+    "url": "/vkstargame-2/static/js/2.7d77f0d6.chunk.js"
+  },
+  {
+    "revision": "2b27df99d06141966c03",
+    "url": "/vkstargame-2/static/js/main.8d8d5471.chunk.js"
+  },
+  {
+    "revision": "cac7295eb93766ab397b",
+    "url": "/vkstargame-2/static/js/runtime-main.213884e3.js"
+  },
+  {
+    "revision": "f2f8376e552608c5fca628e221ee0506",
+    "url": "/vkstargame-2/static/media/sokol.f2f8376e.png"
+  }
+]);
